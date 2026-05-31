b3ea7abeffe8f9c737122308057e3c11a424ac5e
karlos <karlosd832@gmail.com>
2026-05-31 18:35:53 -0300

    feat: telas de login e registro com Thymeleaf

--- src/main/java/com/helpdesk/controller/LoginController.java
+++ src/main/java/com/helpdesk/controller/LoginController.java
@@ -9,4 +9,8 @@ public class LoginController {
     public String login(){
         return "login";
     }
+    @GetMapping("/registro")
+    public String registro() {
+        return "registro";
+    }
 }
